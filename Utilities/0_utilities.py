1d1e03087c94c968718c55b3d09599fae5911edc
Subikesh <subikeshps@gmail.com>
2021-03-16 19:22:17 +0530

    Stack and queue utility python

--- Utilities/0_utilities.py
+++ Utilities/0_utilities.py
@@ -73,3 +73,45 @@ class Linked_list:
                 ptr = ptr.next
             ptr.next = Node(val)
         
+class Stack:
+    def __init__(self):
+        self.head = None
+    
+    def push(self, new_node:Node):
+        if self.head:
+            new_node.next = self.head
+        self.head = new_node
+        return self.head
+    
+    def pop(self):
+        if self.head:
+            popped = self.head
+            self.head = self.head.next
+            popped.next = None
+            return popped.data
+        else:
+            print("Stack Underflow")
+            return None
+
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def push(self, new_node:Node):
+        if self.head == None:
+            self.head = self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return self.tail
+    
+    def pop(self):
+        if self.head:
+            popped = self.head
+            self.head = self.head.next
+            popped.next = None
+            return popped.data
+        else:
+            print("Stack Underflow")
+            return None
